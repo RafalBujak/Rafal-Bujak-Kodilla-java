da27babb31c24fbf38e4970d3ca1d41d6a016d2a
Rafal <r.bujak@somatin.com>
2018-03-26 18:25:15 +0200

    task 2 module 7

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 
 public class StreamMain {
@@ -14,5 +15,8 @@ public class StreamMain {
         poemBeautifier.beautify("I like This Task", String::toLowerCase);
         poemBeautifier.beautify("Si vis pacem, para bellum", (text)->{return "#" + "@" + text + "#" + "@";});
 
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
