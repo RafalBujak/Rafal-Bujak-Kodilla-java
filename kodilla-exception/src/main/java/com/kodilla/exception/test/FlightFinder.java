0436f59665eab1aad22e2baa007acd24d150fd77
Rafal Bujak <bujak.rafal@yahoo.com>
2018-04-04 08:13:12 +0200

    update task flight finder

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -3,43 +3,31 @@ package com.kodilla.exception.test;
 import java.util.*;
 
 public class FlightFinder {
-    Map<String,Boolean> listOfAirports = new HashMap<>();
+    Map<String, Boolean> listOfAirports = new HashMap<>();
 
-    public void findFlight(Flight flight) throws RouteNotFoundException {
+    public boolean findFlight(Flight flight) throws RouteNotFoundException {
 
         listOfAirports.put("DRT", true);
         listOfAirports.put("KAT", true);
         listOfAirports.put("LON", false);
 
-        if(listOfAirports.get(flight.getArrivalAirport())
-                && listOfAirports.get(flight.getDepartureAirport())) {
-            System.out.println("Flight from " + flight.getDepartureAirport()+
-                    " to " + flight.getArrivalAirport() + " has been found!");
-        }
-        else {
+        if (listOfAirports.get(flight.getArrivalAirport())
+                && listOfAirports.get(flight.getDepartureAirport()) == true) {
+            return true;
+        } else {
             throw new RouteNotFoundException();
         }
-
     }
 
-    public void airport(Flight flight) {
-        for (Map.Entry<String, Boolean> entry : listOfAirports.entrySet()) {
-            if(entry.getKey() == flight.getArrivalAirport())
-            System.out.println("Airport: <" + entry.getKey() + ", " + entry.getValue() + ">");
-        }
-    }
-
-
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         FlightFinder flightFinder = new FlightFinder();
 
 
         try {
-            flightFinder.findFlight(new Flight("DRT", "LON"));
-            flightFinder.airport(new Flight("DRT", "LON"));
+            System.out.println("You can fly on this airport: "
+                    + flightFinder.findFlight(new Flight("DRT", "KAT")));
         } catch (RouteNotFoundException e) {
             System.out.println("Route does not exist!");
-            flightFinder.airport(new Flight("DRT", "LON"));
         } finally {
             System.out.println("Find a new route!");
         }
