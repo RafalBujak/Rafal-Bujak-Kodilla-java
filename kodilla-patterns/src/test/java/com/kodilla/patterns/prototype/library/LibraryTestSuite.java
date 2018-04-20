471d77cc2c66d1478200b4e5332eed176dd9feb6
Rafal Bujak <bujak.rafal@yahoo.com>
2018-04-20 16:29:28 +0200

    update 4 task 2

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -40,7 +40,7 @@ public class LibraryTestSuite {
         Assert.assertEquals(5, clonedLibrary.getBooks().size());
         Assert.assertEquals(5, deepClonedLibrary.getBooks().size());
         Assert.assertEquals(clonedLibrary.getBooks(), library.getBooks());
-        Assert.assertNotEquals(deepClonedLibrary, deepClonedLibrary.getBooks());
+        Assert.assertEquals(deepClonedLibrary.getBooks(), library.getBooks());
 
     }
 }
